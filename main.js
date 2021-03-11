a202a1b06059ba84467404f2ec0d7045c7a9164f
Giuditta <giudittamarino89@gmail.com>
2021-03-11 15:32:38 +0100

    c

--- main.js
+++ main.js
@@ -5,14 +5,13 @@ $(document).ready(function(){
 function carousel(){
   // SALVO IN UNA VARIABILE LA FRECCETTA NEXT
   var next = $('.next');
-  console.log(next);
   // SALVO IN UNA VARIABILE LE IMMAGINI
   var images = $('.images img');
 
   var imageA = $('.images img.active');
 
   // AL CLICK SULLA FRECCIA DESTRA DEVO RIMUOVERE LA CLASSE ACTIVE ALLA PRIMA IMMAGINE E AGGIUNGERLA ALLA SECONDA IMMAGINE
-  next.click(function (removeActive) {
+  next.click(function () {
     if (images.hasClass('active')) {
     images.removeClass('active');
     }

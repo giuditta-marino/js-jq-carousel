4fe918ee2d42c484258f0aace99083bf2a57b035
Giuditta <giudittamarino89@gmail.com>
2021-03-11 15:30:33 +0100

    rimossa classe active

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  carousel();
+})
+
+function carousel(){
+  // SALVO IN UNA VARIABILE LA FRECCETTA NEXT
+  var next = $('.next');
+  console.log(next);
+  // SALVO IN UNA VARIABILE LE IMMAGINI
+  var images = $('.images img');
+
+  var imageA = $('.images img.active');
+
+  // AL CLICK SULLA FRECCIA DESTRA DEVO RIMUOVERE LA CLASSE ACTIVE ALLA PRIMA IMMAGINE E AGGIUNGERLA ALLA SECONDA IMMAGINE
+  next.click(function (removeActive) {
+    if (images.hasClass('active')) {
+    images.removeClass('active');
+    }
+  })
+
+
+}
